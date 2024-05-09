6e5ee19d335287f82dc6a392e9fff9e338055e1b
TournierSebastian <110041924+TournierSebastian@users.noreply.github.com>
2024-05-09 14:01:05 -0300

    tarea completa

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import "bootstrap/dist/css/bootstrap.min.css";
-import { LoginForm } from "./components/LoginForm";
+import { LoginForm } from "./entities/Login/LoginForm";
+import ProductList from "./entities/Products/ProductsLis";
+import CreateProduct from "./entities/Products/CreateProduct";
 
 const App = () => {
   return (
     <>
-      <LoginForm />
+      <div>
+        <div style={{ width: '300px' }}>
+          <LoginForm />
+          
+        </div>
+        <ProductList />
+        <CreateProduct></CreateProduct>
+      </div>
     </>
   );
 }
---
+++ src/entities/Products/product-service.jsx
@@ -0,0 +1,16 @@
+
+import api from '../../api';
+
+export const getAllProducts = () => { return api.get("/products")}
+
+export const getProductById = (productId) => {
+    return api.get(`/products/${productId}`);}
+
+export const addProduct = (productData) => {
+    return api.post("/products", productData);}
+
+export const updateProduct = (productId, productData) => {
+    return api.put(`/products/${productId}`, productData);} 
+
+export const deleteProduct = (productId) => {
+    return api.delete(`/products/${productId}`);}
