abf6f874df3824bcbc92b7994599d5cbb3d2df20
TomiRonco <roncoronit@gmail.com>
2024-05-10 17:42:03 -0300

    modulariozacion

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import "bootstrap/dist/css/bootstrap.min.css";
-import { LoginForm } from "./components/LoginForm";
+// import { LoginForm } from "./entities/Login/LoginForm";
+import ProductList from "./entities/Products/ProductsLis";
 
 const App = () => {
   return (
     <>
-      <LoginForm />
+      <div
+        className="d-flex justify-content-center align-items-center"
+        style={{ height: "100vh" }}
+      >
+        <div style={{ width: "300px" }}>{/* <LoginForm /> */}</div>
+        <div style={{ height: "80%", width: "60%" }}>
+          <ProductList />
+        </div>
+      </div>
     </>
   );
-}
+};
 
 export default App;
