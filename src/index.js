803829e8cce19ec04596e089a730b0ab881eaf7e
Frantz Moise Dejoie <amentha2908@gmail.com>
2024-05-09 22:45:15 -0300

    [Amentha Seide][StoreProduct]

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import './index.css';
 import App from './App';
 import { Formik } from 'formik';
 
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
